16b8ad06ca1c00abf3b88b9d653110700d9ffc23
sscrazyy <sscrazyy@gmail.com>
2020-06-16 16:38:36 +0200

    First part of FlaskForms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -11,3 +12,9 @@ def index():
         {"author": {"username": "Pista"}, "body": {"Ez aztan nagyon fasza!"}},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
